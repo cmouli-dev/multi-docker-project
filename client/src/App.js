f671073abba503d8fc1b05f6d7811ac0bd2101e9
cmouli-dev <chandra.mouli.soft.eng@gmail.com>
2025-07-22 11:45:07 +0000

    minor update

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Find the FIBONACI value based on given index
+            Find the FIBONACI value based on given Index
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
